963d5aaa4004b6b2c3957970d7490037d7af0b7f
Chen Xiaoqi <chenxq13@mails.tsinghua.edu.cn>
2015-12-29 19:12:40 -0500

    Code formatting

--- Client.cpp
+++ Client.cpp
@@ -28,69 +28,62 @@ using namespace cv;
 #include "config.h"
 
 
-int main(int argc, char *argv[]) {
-  if ((argc < 3) || (argc > 3)) {   // Test for correct number of arguments
-    cerr << "Usage: " << argv[0] 
-         << " <Server> <Server Port>\n";
-    exit(1);
-  }
-
-  string servAddress = argv[1];             // First arg: server address
-  unsigned short servPort = Socket::resolveService(argv[2], "udp");
-
-  try {
-    UDPSocket sock;
-    int jpegqual=80;
-  
-    Mat frame, send;
-    vector<uchar> encoded;
-    VideoCapture cap(0);//get the camera
-    namedWindow("send",CV_WINDOW_AUTOSIZE);
-    if(!cap.isOpened()){
-      cerr << "OpenCV Failed to open camera";
-      exit(1);
+int main(int argc, char * argv[]) {
+    if ((argc < 3) || (argc > 3)) { // Test for correct number of arguments
+        cerr << "Usage: " << argv[0] << " <Server> <Server Port>\n";
+        exit(1);
     }
 
-    clock_t last_cycle=clock();
-    while(1){
-      cap >> frame;
-      resize(frame, send, Size(FRAME_WIDTH,FRAME_HEIGHT), 0, 0, INTER_LINEAR);
-      vector<int> compression_params;
-      compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
-      compression_params.push_back(jpegqual);
+    string servAddress = argv[1]; // First arg: server address
+    unsigned short servPort = Socket::resolveService(argv[2], "udp");
 
-      imencode(".jpg", send, encoded, compression_params);
-      imshow("send", send);
-      cout<<"sizeof encd:"<<encoded.size()<<endl;
-      //if(encoded.size()>8190)//too big for udp! //shouldn't it bt 65535?
-      //{
-      //  cout<<"reducing jpeg quality..."<<jpegqual<<endl;
-      //  jpegqual--;continue;
-      //}
-      //sock.sendTo(&encoded.front(), encoded.size(), servAddress, servPort);
-      int total_pack=1+(encoded.size()-1)/PACK_SIZE;
+    try {
+        UDPSocket sock;
+        int jpegqual = 80; // Compression Parameter
 
-      int  ibuf[1];ibuf[0]=total_pack;
-      sock.sendTo(ibuf, sizeof(int), servAddress, servPort);
+        Mat frame, send;
+        vector < uchar > encoded;
+        VideoCapture cap(0); // Grab the camera
+        namedWindow("send", CV_WINDOW_AUTOSIZE);
+        if (!cap.isOpened()) {
+            cerr << "OpenCV Failed to open camera";
+            exit(1);
+        }
 
-      for(int i=0;i<total_pack;i++)
-        sock.sendTo(&encoded[i*PACK_SIZE], PACK_SIZE, servAddress, servPort);
-      
-      waitKey(FRAME_INTERVAL);
+        clock_t last_cycle = clock();
+        while (1) {
+            cap >> frame;
+            resize(frame, send, Size(FRAME_WIDTH, FRAME_HEIGHT), 0, 0, INTER_LINEAR);
+            vector < int > compression_params;
+            compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+            compression_params.push_back(jpegqual);
 
-      clock_t next_cycle=clock();
-      double duration=(next_cycle-last_cycle) / (double) CLOCKS_PER_SEC;   
-      cout <<"\teffective FPS:" <<(1/duration)<<" \tkbps:"<<(PACK_SIZE*total_pack/duration/1024*8)<<endl;
-      
-      cout<<next_cycle-last_cycle;
-      last_cycle=next_cycle;
+            imencode(".jpg", send, encoded, compression_params);
+            imshow("send", send);
+            int total_pack = 1 + (encoded.size() - 1) / PACK_SIZE;
+
+            int ibuf[1];
+            ibuf[0] = total_pack;
+            sock.sendTo(ibuf, sizeof(int), servAddress, servPort);
+
+            for (int i = 0; i < total_pack; i++)
+                sock.sendTo( & encoded[i * PACK_SIZE], PACK_SIZE, servAddress, servPort);
+
+            waitKey(FRAME_INTERVAL);
+
+            clock_t next_cycle = clock();
+            double duration = (next_cycle - last_cycle) / (double) CLOCKS_PER_SEC;
+            cout << "\teffective FPS:" << (1 / duration) << " \tkbps:" << (PACK_SIZE * total_pack / duration / 1024 * 8) << endl;
+
+            cout << next_cycle - last_cycle;
+            last_cycle = next_cycle;
+        }
+        // Destructor closes the socket
+
+    } catch (SocketException & e) {
+        cerr << e.what() << endl;
+        exit(1);
     }
-    // Destructor closes the socket
 
-  } catch (SocketException &e) {
-    cerr << e.what() << endl;
-    exit(1);
-  }
-  
-  return 0;
+    return 0;
 }
--- Server.cpp
+++ Server.cpp
@@ -21,85 +21,73 @@
 #include <iostream>          // For cout and cerr
 #include <cstdlib>           // For atoi()
 
-#define BUF_LEN 65540
+#define BUF_LEN 65540 // Larger than maximum UDP packet size
 
 #include "opencv2/opencv.hpp"
 using namespace cv;
 #include "config.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char * argv[]) {
 
-  if (argc != 2) {                  // Test for correct number of parameters
-    cerr << "Usage: " << argv[0] << " <Server Port>" << endl;
-    exit(1);
-  }
-
-  unsigned short servPort = atoi(argv[1]);     // First arg:  local port
-
-  namedWindow("recv",CV_WINDOW_AUTOSIZE);
-  try {
-    UDPSocket sock(servPort);                
-  
-    char buffer[BUF_LEN];         // Buffer for echo string
-    int recvMsgSize;                  // Size of received message
-    string sourceAddress;             // Address of datagram source
-    unsigned short sourcePort;        // Port of datagram source
-    
-    clock_t last_cycle=clock();
-
-    for (;;) {  // Run forever
-      // Block until receive message from a client
-      do{
-        recvMsgSize = sock.recvFrom(buffer, BUF_LEN, sourceAddress, sourcePort);
-      }while(recvMsgSize>sizeof(int));
-      int total_pack=((int*) buffer)[0];
-
-      cout<<"expecting length of packs:"<<total_pack<<endl;
-      char* longbuf=new char[PACK_SIZE*total_pack];
-      for(int i=0;i<total_pack;i++){
-        recvMsgSize = sock.recvFrom(buffer, BUF_LEN, sourceAddress, sourcePort);
-        if(recvMsgSize != PACK_SIZE){
-          cerr << "Received unexpected size pack:" << recvMsgSize << endl;
-          continue;
-        }
-        memcpy(&longbuf[i*PACK_SIZE], buffer, PACK_SIZE);
-      }
-
-
-
-      cout << "Received packet from " << sourceAddress << ":" 
-        << sourcePort << endl;
-      //cout << "Length:" << recvMsgSize << "w/ first Char:" 
-      //  << buffer[0] <<endl;
-  
-      //for (int i=0;;i++){
-      //  sprintf(buffer,"seq: %d:\n",i);
-      //  sock.sendTo(buffer, strlen(buffer), sourceAddress, sourcePort);
-      //  sleep(1);
-        //cout<<ret<<endl;
-      //}  
-      Mat rawData = Mat( 1, PACK_SIZE*total_pack, CV_8UC1, longbuf );
-      Mat frame = imdecode(rawData, CV_LOAD_IMAGE_COLOR);
-      if(frame.size().width==0){
-        cerr<< "decode failure!" <<endl;
-        continue;
-      }
-      imshow("recv", frame);
-      free(longbuf);
-
-      waitKey(1);
-      clock_t next_cycle=clock();
-      double duration=(next_cycle-last_cycle) / (double) CLOCKS_PER_SEC;   
-      cout <<"\teffective FPS:" <<(1/duration)<<" \tkbps:"<<(PACK_SIZE*total_pack/duration/1024*8)<<endl;
-      
-      cout<<next_cycle-last_cycle;
-      last_cycle=next_cycle;
+    if (argc != 2) { // Test for correct number of parameters
+        cerr << "Usage: " << argv[0] << " <Server Port>" << endl;
+        exit(1);
     }
-  } catch (SocketException &e) {
-    cerr << e.what() << endl;
-    exit(1);
-  }
-  // NOT REACHED
 
-  return 0;
+    unsigned short servPort = atoi(argv[1]); // First arg:  local port
+
+    namedWindow("recv", CV_WINDOW_AUTOSIZE);
+    try {
+        UDPSocket sock(servPort);
+
+        char buffer[BUF_LEN]; // Buffer for echo string
+        int recvMsgSize; // Size of received message
+        string sourceAddress; // Address of datagram source
+        unsigned short sourcePort; // Port of datagram source
+
+        clock_t last_cycle = clock();
+
+        while (1) {
+            // Block until receive message from a client
+            do {
+                recvMsgSize = sock.recvFrom(buffer, BUF_LEN, sourceAddress, sourcePort);
+            } while (recvMsgSize > sizeof(int));
+            int total_pack = ((int * ) buffer)[0];
+
+            cout << "expecting length of packs:" << total_pack << endl;
+            char * longbuf = new char[PACK_SIZE * total_pack];
+            for (int i = 0; i < total_pack; i++) {
+                recvMsgSize = sock.recvFrom(buffer, BUF_LEN, sourceAddress, sourcePort);
+                if (recvMsgSize != PACK_SIZE) {
+                    cerr << "Received unexpected size pack:" << recvMsgSize << endl;
+                    continue;
+                }
+                memcpy( & longbuf[i * PACK_SIZE], buffer, PACK_SIZE);
+            }
+
+            cout << "Received packet from " << sourceAddress << ":" << sourcePort << endl;
+ 
+            Mat rawData = Mat(1, PACK_SIZE * total_pack, CV_8UC1, longbuf);
+            Mat frame = imdecode(rawData, CV_LOAD_IMAGE_COLOR);
+            if (frame.size().width == 0) {
+                cerr << "decode failure!" << endl;
+                continue;
+            }
+            imshow("recv", frame);
+            free(longbuf);
+
+            waitKey(1);
+            clock_t next_cycle = clock();
+            double duration = (next_cycle - last_cycle) / (double) CLOCKS_PER_SEC;
+            cout << "\teffective FPS:" << (1 / duration) << " \tkbps:" << (PACK_SIZE * total_pack / duration / 1024 * 8) << endl;
+
+            cout << next_cycle - last_cycle;
+            last_cycle = next_cycle;
+        }
+    } catch (SocketException & e) {
+        cerr << e.what() << endl;
+        exit(1);
+    }
+
+    return 0;
 }
